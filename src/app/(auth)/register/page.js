c3d3552f858e1fefcd1cf7a21bd87e1e50e00070
Anuroop2111 <anuroopls2001@gmail.com>
2024-06-05 04:20:00 +0530

    first commit

---
+++ src/app/(auth)/register/page.js
@@ -0,0 +1,82 @@
+"use client";
+
+import "../style.css";
+import { useRouter } from 'next/navigation';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import Link from "next/link";
+
+const Login = () => {
+    const router = useRouter();
+    
+    const initialValues = {
+        email: '',
+        password: ''
+    };
+
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .required('Email is required')
+            .email("Invalid email address"),
+
+        password: Yup.string()
+            .required('Password is required')
+            .min(8, "Password must be at least 8 characters"),
+    });
+
+
+    const handleSubmit = (values) => {
+        console.log('Form data', values);
+
+        // If valid login, navigate to home page
+        router.push("/home");   
+    }
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Register</h2>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {formik => (
+                        <Form>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <Field
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    className="form-control"
+                                    placeholder="Enter email address"
+                                />
+                                <ErrorMessage name="email" component="span" className="error-message" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <Field
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    className="form-control"
+                                    placeholder="Enter password"
+                                />
+                                <ErrorMessage name="password" component="span" className="error-message" />
+                            </div>
+                            <button type="submit" className="btn btn-primary">
+                                Sign Up
+                            </button>
+
+                            <div className="additional-link">
+                            <p>Already have an account? <Link href="/login">Login</Link></p>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
