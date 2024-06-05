3826d0249361d2df40e03b4d42efeafc367ddd6c
Anuroop2111 <anuroopls2001@gmail.com>
2024-06-05 23:21:22 +0530

    visualising card component in Home component

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,5 +1,35 @@
-export default function Home () {
+"use client";
+
+import React from "react";
+import Card from "../components/card/card";
+import style from "./style.css";
+
+const Home = () => {
+    const todos = [
+        { name: 'Work', completed: 5, total: 11 },
+        { name: 'Groceries', completed: 2, total: 5 },
+        // Add more todo items here
+      ];
+
     return (
+        <>
         <h1>Welcome Home</h1>
+        <div className="container"> 
+            <h1>TODO List</h1>
+            <div>
+                {todos.map((todo, index) => (
+                <Card
+                    key={index}
+                    name={todo.name}
+                    completed={todo.completed}
+                    total={todo.total}
+                />
+                ))}
+            </div>
+        </div>
+        </>
+        
     )
 }
+
+export default Home;
